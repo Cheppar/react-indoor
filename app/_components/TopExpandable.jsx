2ce7969171e98eafee53f5f552b86aa48bf85aed
makenaejiofor <ejioforched@gmail.com>
2024-05-26 05:37:05 +0300

    added Cmd panel to drop down menu

--- app/_components/TopExpandable.jsx
+++ app/_components/TopExpandable.jsx
@@ -12,6 +12,7 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
+import Cmd from './Cmd';
 
 // Define the ListItem component
 const ListItem = ({ href, title, children }) => (
@@ -68,34 +69,8 @@ function TopExpandable() {
           <NavigationMenuItem>
             <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <li className="row-span-3">
-                  <NavigationMenuLink asChild>
-                    <a
-                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                      href="/"
-                    >
-                        
-                      <ChevronDown className="h-6 w-6" />
-                      <div className="mb-2 mt-4 text-lg font-medium">
-                        shadcn/ui
-                      </div>
-                      <p className="text-sm leading-tight text-muted-foreground">
-                        Beautifully designed components built with Radix UI and Tailwind CSS.
-                      </p>
-                    </a>
-                  </NavigationMenuLink>
-                </li>
-                {/* Placeholder list items */}
-                <ListItem href="/docs" title="Introduction">
-                  Re-usable components built using Radix UI and Tailwind CSS.
-                </ListItem>
-                <ListItem href="/docs/installation" title="Installation">
-                  How to install dependencies and structure your app.
-                </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Typography">
-                  Styles for headings, paragraphs, lists...etc
-                </ListItem>
+              <ul className="grid gap-3 md:w-[300px] lg:w-[300px] lg:grid-cols-[.75fr_1fr]">
+                <Cmd />
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
